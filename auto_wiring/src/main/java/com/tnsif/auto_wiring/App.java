543d20a0afdc68ce5133e09178adc5f812ba5a0a
Abinaya <Abinaya@LAPTOP-NTETH36S>
2025-11-06 21:45:34 +0530

    commit

---
+++ auto_wiring/src/main/java/com/tnsif/auto_wiring/App.java
@@ -0,0 +1,13 @@
+package com.tnsif.auto_wiring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
